d727923cfdc3dd3861cdd8d9394b15e0ac37c625
caesar <linmomo91@outlook.com>
2018-06-27 17:12:30 +0800

    mongoose queries and ID Validation

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,12 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text:'First test todo'
+},{
+  text:'Second test todo'
+}];
+
 beforeEach((done) => {
   Todo.remove({}).then(() => done());
 });
@@ -46,5 +52,17 @@ describe('POST/todos',() => {
         done();
       }).catch((e) => done(e));
     });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos',(done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
   });
 });
