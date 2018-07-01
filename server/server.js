8f9e213015ed0c9a7c7f47faf8aed3df6d47d968
caesar <linmomo91@outlook.com>
2018-07-01 10:52:42 +0800

    Add script

--- server/server.js
+++ server/server.js
@@ -63,6 +63,7 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
@@ -97,7 +98,7 @@ app.patch('/todos/:id', (req, res) => {
 
 // POST /users
 app.post('/users', (req, res) => {
-  var body = _.pick(req.body,['email','password']);
+  var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
   user.save().then(() => {
